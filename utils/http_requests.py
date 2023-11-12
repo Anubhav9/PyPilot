be006b184c664647b2801eff3d9741d4f0475de4
Anubhav Sanyal <princebest3@rediffmail.com>
2023-11-12 20:14:41 +0530

    Added DocString in functions

--- utils/http_requests.py
+++ utils/http_requests.py
@@ -2,31 +2,33 @@ import logging
 import requests
 import utility_helpers
 
-"""
-Utility Method to invoke get request by constructing the URL at run time
 
-Parameters
-service_name (string): Name of the already onboarded Service
-end_point (string): End Point that we want to invoke
-headers : header like Authorisation etc, if any
-is_secured (boolean): Parameter to set if we want to invoke http or https
-"""
 def invoke_get_request(service_name,end_point,headers=None,is_secured=True):
+    """
+    Utility Method to invoke get request by constructing the URL at run time
+
+    Parameters
+    service_name (string): Name of the already onboarded Service
+    end_point (string): End Point that we want to invoke
+    headers : header like Authorisation etc, if any
+    is_secured (boolean): Parameter to set if we want to invoke http or https
+    """
     generated_url=utility_helpers.generate_url_for_request(service_name,end_point,is_secured)
     response=requests.get(generated_url,headers=headers)
     return response
 
-"""
-Utility Method to invoke post request by constructing the URL at run time
 
-Parameters
-service_name (string): Name of the already onboarded Service
-end_point (string): End Point that we want to invoke
-body (json): Constructing the body request at run time
-headers : header like Authorisation etc, if any
-is_secured (boolean): Parameter to set if we want to invoke http or https
-"""
 def invoke_post_request(service_name, end_point, body, headers=None,is_secured=True):
+    """
+    Utility Method to invoke post request by constructing the URL at run time
+
+    Parameters
+    service_name (string): Name of the already onboarded Service
+    end_point (string): End Point that we want to invoke
+    body (json): Constructing the body request at run time
+    headers : header like Authorisation etc, if any
+    is_secured (boolean): Parameter to set if we want to invoke http or https
+    """
     generated_url = utility_helpers.generate_url_for_request(service_name, end_point,is_secured)
     if not utility_helpers.is_valid_json(body):
         raise ValueError("Provided body is not a valid JSON")
--- utils/utility_helpers.py
+++ utils/utility_helpers.py
@@ -1,15 +1,15 @@
 import json
 from utility_constants import *
 
-"""
-Utility Method to generate complete url of the request to be invoked while calling the API
-
-Parameters
-service_name (string): Name of the already onboarded Service
-end_point (string): End Point that we want to invoke
-is_secured (boolean): Parameter to set if we want to invoke http or https
-"""
 def generate_url_for_request(service_name,end_point,is_secured_protocol):
+    """
+    Utility Method to generate complete url of the request to be invoked while calling the API
+
+    Parameters
+    service_name (string): Name of the already onboarded Service
+    end_point (string): End Point that we want to invoke
+    is_secured (boolean): Parameter to set if we want to invoke http or https
+    """
     if(is_secured_protocol==True):
         generated_url=HTTPS_PROTOCOL+service_name+SLASH+end_point
         
@@ -18,13 +18,14 @@ def generate_url_for_request(service_name,end_point,is_secured_protocol):
     
     return generated_url
 
-"""
-Utility Method to check if the json passed is a valid JSON or not
 
-Parameters
-suspect_string (json): Mostly the body to be passed in Post Request
-"""
 def is_valid_json(suspect_string):
+    """
+    Utility Method to check if the json passed is a valid JSON or not
+
+    Parameters
+    suspect_string (json): Mostly the body to be passed in Post Request
+    """
     try:
         json.loads(suspect_string)
         return True
@@ -32,15 +33,15 @@ def is_valid_json(suspect_string):
         return False
     
         
-"""
-Utility Method to replace values at run time. Usually in api calls where methods are placeholders.
-
-Parameters
-constant_name (string): The constant string where placeholder has been stored
-placeholder (string): Passing the placeholder value
-value (string): The value to be replaced for in placeholder
-"""
 
 def replace_placeholder(constant_name,placeholder,value):
+    """
+    Utility Method to replace values at run time. Usually in api calls where methods are placeholders.
+
+    Parameters
+    constant_name (string): The constant string where placeholder has been stored
+    placeholder (string): Passing the placeholder value
+    value (string): The value to be replaced for in placeholder
+    """
     replace_string=constant_name.replace(placeholder,value)
     return replace_string
