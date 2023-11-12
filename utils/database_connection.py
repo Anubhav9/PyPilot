e255842bd095b9a1bb17a719e0fbdd8072020bf1
Anubhav Sanyal <princebest3@rediffmail.com>
2023-11-12 23:21:53 +0530

    Utility Added For Connection with DB

---
+++ utils/database_connection.py
@@ -0,0 +1,60 @@
+"""
+Utility methods for some of the most common database interactions (SQL)
+
+Author: Anubhav Sanyal
+Creation Date: 12/11/2023
+"""
+
+import logging
+import os
+from mysql.connector import pooling
+import json
+
+connection=None
+dbconfig_global=None
+
+def establish_pool_connection(dbconfig_json,service_database_name):
+    """
+    Utility Method to establish PoolConnection with the Database
+
+    Parameters
+    dbconfig_json (json): JSON file containing DB Configuration
+    service_database_name (string): Filter Out DB Details using this value
+    """
+    dbconfig=dbconfig_json[service_database_name]
+    try:
+        connection_pool=pooling.MySQLConnectionPool(pool_name="sql-pool",pool_size=10,**dbconfig)
+        connection=connection_pool.get_connection()
+        return connection
+    except Exception as e:
+        logging.error("Unable to establish pool connection due to error %s",e)
+
+
+def execute_query(sql_query,params=None):
+    """
+    Utility Method to execute query in the database
+
+    Parameters
+    sql_query (string): SQL Query with Parameters to avoid SQL Injection
+    params : Parameters passed to the query (optional)
+    """
+    try:
+        cursor=connection.cursor()
+        cursor.execute(sql_query,params)
+        connection.commit()
+    except Exception as e:
+        logging.error("Unable to connect to MySQL DB with error as %s",e)
+
+
+
+env=os.environ.get("ENV","local")
+if env=="local":
+    try:
+        with open("../databases/database_config_local.json", 'r') as file:
+            file_content=file.read()
+            file_content=file_content.replace("{SENDGRID_DATABASE_PASSWORD}",os.environ.get("SENDGRAID_DATABASE_PASSWORD"))
+            dbconfig_global=json.load(file_content)
+    except Exception as e:
+        logging.error("Unable to open file because of Error as %s",e)
+
+connection=establish_pool_connection(dbconfig_global,"SENDGRID_SQL_DB")
