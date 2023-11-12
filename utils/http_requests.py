c265d6476dac53f98c728b4466183d1e5716a348
Anubhav Sanyal <princebest3@rediffmail.com>
2023-11-12 15:16:16 +0530

    default parameters set

--- utils/http_requests.py
+++ utils/http_requests.py
@@ -3,8 +3,8 @@ import logging
 import utility_helpers
 
 
-def invoke_get_request(service_name,end_point,headers=None):
-    generated_url=utility_helpers.generate_url_for_request(service_name,end_point)
+def invoke_get_request(service_name,end_point,headers=None,is_secured=True):
+    generated_url=utility_helpers.generate_url_for_request(service_name,end_point,is_secured)
     if(headers is not None):
         response=requests.get(generated_url,headers=headers)
     else:
@@ -12,8 +12,8 @@ def invoke_get_request(service_name,end_point,headers=None):
     return response
 
 
-def invoke_post_request(service_name, end_point, body, headers=None):
-    generated_url = utility_helpers.generate_url_for_request(service_name, end_point)
+def invoke_post_request(service_name, end_point, body, headers=None,is_secured=True):
+    generated_url = utility_helpers.generate_url_for_request(service_name, end_point,is_secured)
     if not utility_helpers.is_valid_json(body):
         raise ValueError("Provided body is not a valid JSON")
 
